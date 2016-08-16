19daadddfa15c1b0e20b810663c37c1bd21a88f3
Tim Garcia <Tim Garcia>
2016-08-16 17:26:56 +0800

    just having fun

--- index.php
+++ index.php
@@ -1,13 +1,57 @@
 <?php
-	//$xml = new DomDocument;
-	//$xml->Load('C:\xampp\htdocs\SmsReader\SmsConvo.xml');
-	$xml=simplexml_load_file("C:\Users\Tim Garcia\Documents\Web\SmsConvo.xml") or die("Error: Cannot create object");
+	//$xml=simplexml_load_file("C:\Users\Tim Garcia\Documents\Web\SmsConvo.xml") or die("Error: Cannot create object");
 
 
-	foreach($xml->children() as $sms) { 
-    echo $sms['address'] . ' ' . $sms['time'] . ' ' . $sms['date'] . ' ' . $sms['type'] . ' ' . $sms['body'] . ' ' . $sms['name'];
-    echo "<br>"; 
+	//foreach($xml->children() as $sms) { 
+    //echo $sms['address'] . ' ' . $sms['time'] . ' ' . $sms['date'] . ' ' . $sms['type'] . ' ' . $sms['body'] . ' ' . $sms['name'];
+    //echo "<br>"; 
 	
-	}
+	//}
 
 ?>
+
+<!DOCTYPE html> 
+<html lang="en">
+	<head>
+		<title>Sms Reader</title>
+		<link rel="stylesheet" href="main.css">
+
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+	</head>
+
+
+
+	<body>
+
+		<header>
+			<nav class="navbar navbar-inverse">
+  			<div class="container-fluid">
+    			<div class="navbar-header">
+      				<a class="navbar-brand" href="#">
+        				<img alt="Brand" src="logo.png" width="50px" height="25px">
+      				</a>
+    			</div>
+  			</div>
+			</nav>
+
+		</header>
+
+
+
+
+		<div class="container">
+			<div class="row">
+				<div class="darkBG col-sn-6">test</div>
+				<div class="darkerBG col-sn-6">test</div>
+				<div class="darkestBG col-sn-6">test</div>
+			</div>
+		</div>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	</body>
+</html>
